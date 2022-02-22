182efcab4434b2efde6597d3f5627bba9fe4c304
Shailendra Pratap Singh <84892383+shailendrapr@users.noreply.github.com>
2022-02-22 23:44:20 +0530

    Time: 2 ms (31.45%), Space: 5.8 MB (68.50%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int lo=0,hi=n;
+        while(lo<hi){int mid=lo+(hi-lo)/2;
+           if(!isBadVersion(mid)) lo = mid + 1;  
+            else hi = mid;
+        }
+        return hi;  
+    }
+};
