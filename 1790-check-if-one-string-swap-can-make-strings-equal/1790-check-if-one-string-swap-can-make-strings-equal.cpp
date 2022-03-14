c181d4da82880ef58cdf22410b9c1e0f015502e5
Shailendra Pratap Singh <84892383+shailendrapr@users.noreply.github.com>
2022-03-14 18:26:16 +0530

    Time: 5 ms (22.66%), Space: 6.4 MB (26.66%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,17 +1,40 @@
-
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        vector<int> diff_pos;
-        for(int i = 0; i < s1.size(); i++) {
-            if(s1[i] != s2[i])
-                diff_pos.emplace_back(i);
-        }
-        if(diff_pos.empty())
-            return true;
-        if(diff_pos.size() == 2) 
-            swap(s1[diff_pos[0]], s1[diff_pos[1]]);
         
-        return s1 == s2;
+        if(s1.size()==1)
+        {
+            return s1==s2;
+        }
+        
+        vector<int> phash(26,0);
+        vector<int> shash(26,0);
+        
+        int count=0;
+        int i=0;
+        int j=0;
+        for(int i=0;i<s1.size();i++)
+        {
+            phash[s1[i]-'a']++;
+            shash[s2[i]-'a']++;
+        }
+        if(phash!=shash)
+        {
+            return false;
+        }
+        else
+        {
+            while(i<s1.size() && j<s2.size() && count<3)
+            {
+                if(s1[i]!=s2[i])
+                {
+                    count++;
+                }
+                i++;
+                j++;
+            }
+        }
+        return count<3;
+        
     }
 };
